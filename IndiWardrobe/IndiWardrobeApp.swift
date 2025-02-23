fea1a13e5c31b91453addfa2f58ac0261845bd50
Vishnu Jay Kumar <vkmr327@gmail.com>
2025-02-23 16:31:27 -0500

    Initial Commit

---
+++ IndiWardrobe/IndiWardrobeApp.swift
@@ -0,0 +1,17 @@
+//
+//  IndiWardrobeApp.swift
+//  IndiWardrobe
+//
+//  Created by Vishnu Kumar on 2/23/25.
+//
+
+import SwiftUI
+
+@main
+struct IndiWardrobeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IndiWardrobeTests/IndiWardrobeTests.swift
@@ -0,0 +1,17 @@
+//
+//  IndiWardrobeTests.swift
+//  IndiWardrobeTests
+//
+//  Created by Vishnu Kumar on 2/23/25.
+//
+
+import Testing
+@testable import IndiWardrobe
+
+struct IndiWardrobeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
